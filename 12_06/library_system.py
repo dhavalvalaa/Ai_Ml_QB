832c6eb23a9a1e31496e9210aef22cc325170f74
dhavalvalaa <dhavalvala.quantumbot@gmail.com>
2025-06-13 10:53:33 +0530

    12_06

--- 12_06/library_system.py
+++ 12_06/library_system.py
@@ -1,18 +1,18 @@
 # Develop a simple library system using classes for `Book`, `Member`, and `Library`,
 # with methods to add/remove books and members.
 
-class book:
+class Book:
     def __init__(self,bid,bname,quanity):
         self.bid = bid
         self.bname = bname
         self.quanity = quanity
 
-class member:
+class Member:
     def __init__(self,mid,mname):
         self.mid = mid
         self.mname = mname
 
-class library:
+class Library:
     def __init__(self):
         self.books = {}
         self.members = {}
@@ -21,7 +21,7 @@ class library:
         self.books[book.bid] = book
 
     def remove_book(self,bid):
-        if bid in self.books:
+        if bid in self.books.keys():
             self.books.pop(bid)
         else:
             print("book name is not valid")
@@ -30,24 +30,46 @@ class library:
         self.members[member.mid] = member
 
     def remove_member(self,mid):
-        if mid in self.members:
+        if mid in self.members.keys():
             self.members.pop(mid)
         else:
             print("member name not valid")
 
     def display_book(self):
         if self.books:
-            print("library books")
-            for book in self.books.values():
+            print("\nlibrary books")
+            for book in self.books:
                 print(book)
         else:
             print("no books in library")
 
     def display_member(self):
         if self.members:
-            print("library members: ")
+            print("\nlibrary members: ")
             for member in self.members:
                 print(member)
         else:
             print("no books in library")
 
+
+library = Library()
+
+
+book1 = Book(101,"aa",10)
+book2 = Book(102,"bb",20)
+library.add_book(book1)
+library.add_book(book2)
+
+member1 = Member(1,"aa")
+member2 = Member(2,"bb")
+library.add_member(member1)
+library.add_member(member2)
+
+library.display_book()
+library.display_member()
+
+library.remove_book(101)
+library.remove_member(1)
+
+library.display_book()
+library.display_member()
